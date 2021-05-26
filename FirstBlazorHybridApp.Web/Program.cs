f2df979035ac8feb978681fe224aa56627fb518d
Blouin Cédric <ced_the_kat@hotmail.com>
2021-05-26 15:55:29 +0200

    Start

--- FirstBlazorHybridApp.Web/Program.cs
+++ FirstBlazorHybridApp.Web/Program.cs
@@ -21,7 +21,7 @@ namespace FirstBlazorHybridApp.Web
 
             //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddSingleton<CounterState>();
-            //builder.Services.AddSingleton<IFileProvider>(new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot")));
+            //builder.Services.AddSingleton<IFileProvider>(new PhysicalFileProvider(Path.Combine(builder.HostEnvironment., @"wwwroot")));
             await builder.Build().RunAsync();
         }
     }
